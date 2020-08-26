7e9d3412e8a1aa49395656b0b6e10416a21f3596
mina ehab <51240120+minaehab1@users.noreply.github.com>
2020-08-27 01:59:30 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<html lang="en">
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body>
+<meta http-equiv="Refresh" content="0; url=https://www.spectrumservices.ae/cleaning-booking/giveme55/" />
+</body>
+</html>
