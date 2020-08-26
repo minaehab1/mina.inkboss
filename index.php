355cbeb0ff8e1ee637ead11d2e0efbbe50d6443a
mina ehab <51240120+minaehab1@users.noreply.github.com>
2020-08-27 02:03:56 +0400

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 </head>
 <body>
-<meta http-equiv="Refresh" content="0; url=https://www.spectrumservices.ae/cleaning-booking/giveme55/" />
+<meta http-equiv="Refresh" content="0; url=https://api.whatsapp.com/send?phone=971586688378&text=Hello,%20I'm%20interested%20about%20the%20current%20offer." />
 </body>
 </html>
